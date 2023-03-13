8fad5a777296ef306335428d9b06cc7eabfd237c
olivebishop <olivehendrilgen1@gmail.com>
2023-03-13 23:58:08 +0300

    added some controllers and models

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-import dotenv from 'dotenv';
 import express from 'express';
-import User from './models/User.js';
-import Role from './models/Role.js';
+import dotenv from 'dotenv';
+
+import userRoutes from './routes/userRoutes.js';
+import roleRoutes from './routes/roleRoutes.js';
+import userRoleRouter from './routes/userRoleRoutes.js';
+import db from './models/index.js';
+
+const sequelize = db.sequelize;
 
 dotenv.config();
 const app = express();
 
+app.use(express.json());
 
-// app.get('/users', (req, res) => {
-// res.status(200).json({message:'Success'})
-// })
+sequelize.sync({alter: true}).then(() => {
+    console.log("Tables created.");
+}).catch(err =>{
+    console.log(err);
+});
 
-const port = process.env.PORT || 5000;
+app.use('/api/v1', userRoutes);
+app.use('/api/v1', roleRoutes);
+app.use('/api/v1', userRoleRouter);
+
+const port = process.env.PORT;
 
 app.listen(port, () => {
-console.log(`Server running on port ${port}`);
+    console.log(`Server started at localhost:${port}`);
 });
+    
--- models/Role.js
+++ models/Role.js
@@ -1,14 +1,28 @@
 import { DataTypes } from 'sequelize';
-import sequelize from '../config/db.js';
 
-const Role = sequelize.define('role', {
-    name: { 
-        type: DataTypes.STRING, 
+// import sequelize from '../config/db.js';
+
+// const Role = sequelize.define('role', {
+
+// name: { 
+//     type: DataTypes.STRING, 
+//     allowNull:false 
+// },
+
+// });
+
+// sequelize.sync();
+
+// export default Role;
+
+const Role = (sequelize, Sequelize) => {
+    const Role = sequelize.define("role", {
+      name: {
+        type: DataTypes.STRING,
         allowNull:false 
-    },
-    
-});
+      }
+    });
+    return Role;
+  };
 
-sequelize.sync();
-
-export default Role;
+  export default Role;
